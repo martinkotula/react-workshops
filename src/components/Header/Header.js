d13989d0fc60ecae27bd13f557809860c5cca5fb
Martin Kotula <marcin.kotula.ctr@sabre.com>
2017-08-24 11:06:16 +0200

    Switch Header to component class

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,42 @@
-import React from 'react'
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Header.css';
 import icon from './download.jpg';
 
-const Header = function ({ children, componentToUse, ComponentToUse }) {
-    let content = children;
-    if (componentToUse) {
+export default class Header extends React.Component {
+
+    constructor() {
+        super();
+
+        this.defaultProps = {
+            children: "!"
+        }
+
+        this.propTypes = {
+            children: PropTypes.string,
+            componentToUse: PropTypes.func,
+            ComponentToUse: PropTypes.func
+        }
+    }
+
+    render() {
+        let { children, componentToUse, ComponentToUse } = this.props;
+        let content = children;
+        if (componentToUse) {
+            content = React.createElement(componentToUse, { text: content })
+        }
+
+        if (ComponentToUse) {
+            content = <ComponentToUse text={content} />
+        }
+
         return (
-            <h1>{React.createElement(componentToUse, { text: content })}</h1>
+            <h1 className="header">
+                <img src={icon} height="32" />
+                {content}
+            </h1>
         )
     }
 
-    if (ComponentToUse) {
-        return (
-            <h1><ComponentToUse text={content} /></h1>
-        )
-    }
-
-    return (
-        <h1 className="header">
-            <img src={icon} height="32" />
-            {content}
-        </h1>
-    )
 }
-
-Header.defaultProps = {
-    children: "!"
-}
-
-Header.propTypes = {
-    children: PropTypes.string,
-    componentToUse: PropTypes.func,
-    ComponentToUse: PropTypes.func
-}
-
-export default Header;
